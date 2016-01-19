97bedcbc81bf4b5fdb4532400bd2a0f15b3d16d6
Michael JasonSmith <mpj17@onlinegroups.net>
2016-01-19 10:04:16 -0800

    Whitespace cleanup, in theory

--- gs/content/js/wymeditor/browser/javascript/1.1.1/jquery.wymeditor.js
+++ gs/content/js/wymeditor/browser/javascript/1.1.1/jquery.wymeditor.js
@@ -16027,157 +16027,157 @@ function apply(changes, target, modify) {
 
 },{}],15:[function(require,module,exports){
 (function (root, factory) {
-		"use strict";
+                "use strict";
 
-		if (typeof exports === 'object') {
-			module.exports = factory();
-		} else if (typeof define === 'function' && define.amd) {
-			define(factory);
-		} else {
-			root.UltraDeepClone = factory();
-		}
-	}(this, function () {
+                if (typeof exports === 'object') {
+                        module.exports = factory();
+                } else if (typeof define === 'function' && define.amd) {
+                        define(factory);
+                } else {
+                        root.UltraDeepClone = factory();
+                }
+        }(this, function () {
 
-		var functionPropertyFilter = [
-			"caller",
-			"arguments"
-		];
+                var functionPropertyFilter = [
+                        "caller",
+                        "arguments"
+                ];
 
-		// Node.js has a lot of silly enumeral properties on its "TypedArray" implementation
-		var typedArrayPropertyFilter = [
-			'BYTES_PER_ELEMENT',
-			'get',
-			'set',
-			'slice',
-			'subarray',
-			'buffer',
-			'length',
-			'byteOffset',
-			'byteLength'
-		];
+                // Node.js has a lot of silly enumeral properties on its "TypedArray" implementation
+                var typedArrayPropertyFilter = [
+                        'BYTES_PER_ELEMENT',
+                        'get',
+                        'set',
+                        'slice',
+                        'subarray',
+                        'buffer',
+                        'length',
+                        'byteOffset',
+                        'byteLength'
+                ];
 
-		var primitiveCloner  = makeCloner(clonePrimitive);
-		var typedArrayCloner = makeRecursiveCloner(makeCloner(cloneTypedArray), typedArrayPropertyFilter);
+                var primitiveCloner  = makeCloner(clonePrimitive);
+                var typedArrayCloner = makeRecursiveCloner(makeCloner(cloneTypedArray), typedArrayPropertyFilter);
 
-		function typeString (type) {
-			return '[object ' + type + ']';
-		}
+                function typeString (type) {
+                        return '[object ' + type + ']';
+                }
 
-		var cloneFunctions = {};
+                var cloneFunctions = {};
 
-		cloneFunctions[typeString('RegExp')] = makeCloner(cloneRegExp);
-		cloneFunctions[typeString('Date')] = makeCloner(cloneDate);
-		cloneFunctions[typeString('Function')] = makeRecursiveCloner(makeCloner(cloneFunction), functionPropertyFilter);
-		cloneFunctions[typeString('Object')] = makeRecursiveCloner(makeCloner(cloneObject));
-		cloneFunctions[typeString('Array')] = makeRecursiveCloner(makeCloner(cloneArray));
+                cloneFunctions[typeString('RegExp')] = makeCloner(cloneRegExp);
+                cloneFunctions[typeString('Date')] = makeCloner(cloneDate);
+                cloneFunctions[typeString('Function')] = makeRecursiveCloner(makeCloner(cloneFunction), functionPropertyFilter);
+                cloneFunctions[typeString('Object')] = makeRecursiveCloner(makeCloner(cloneObject));
+                cloneFunctions[typeString('Array')] = makeRecursiveCloner(makeCloner(cloneArray));
 
-		['Null', 'Undefined', 'Number', 'String', 'Boolean']
-			.map(typeString)
-			.forEach(function (type) {
-				cloneFunctions[type] = primitiveCloner;
-			});
+                ['Null', 'Undefined', 'Number', 'String', 'Boolean']
+                        .map(typeString)
+                        .forEach(function (type) {
+                                cloneFunctions[type] = primitiveCloner;
+                        });
 
-		['Int8Array', 'Uint8Array', 'Uint8ClampedArray', 'Int16Array', 'Uint16Array',
-		 'Int32Array', 'Uint32Array', 'Float32Array', 'Float64Array']
-			.map(typeString)
-			.forEach(function (type) {
-				cloneFunctions[type] = typedArrayCloner;
-			});
+                ['Int8Array', 'Uint8Array', 'Uint8ClampedArray', 'Int16Array', 'Uint16Array',
+                 'Int32Array', 'Uint32Array', 'Float32Array', 'Float64Array']
+                        .map(typeString)
+                        .forEach(function (type) {
+                                cloneFunctions[type] = typedArrayCloner;
+                        });
 
-		function makeArguments (numberOfArgs) {
-			var letters = [];
-			for ( var i = 1; i <= numberOfArgs; i++ ) letters.push("arg" + i);
-			return letters;
-		}
+                function makeArguments (numberOfArgs) {
+                        var letters = [];
+                        for ( var i = 1; i <= numberOfArgs; i++ ) letters.push("arg" + i);
+                        return letters;
+                }
 
-		function wrapFunctionWithArity (callback) {
-			var argList = makeArguments(callback.length);
-			var functionCode = 'return false || function ';
-			functionCode += callback.name + '(';
-			functionCode += argList.join(', ') + ') {\n';
-			functionCode += 'return fn.apply(this, arguments);\n';
-			functionCode += '};'
+                function wrapFunctionWithArity (callback) {
+                        var argList = makeArguments(callback.length);
+                        var functionCode = 'return false || function ';
+                        functionCode += callback.name + '(';
+                        functionCode += argList.join(', ') + ') {\n';
+                        functionCode += 'return fn.apply(this, arguments);\n';
+                        functionCode += '};'
 
-			return Function("fn", functionCode)(callback);
-		}
+                        return Function("fn", functionCode)(callback);
+                }
 
-		function makeCloner (cloneThing) {
-			return function(thing, thingStack, copyStack) {
-				thingStack.push(thing);
-				var copy = cloneThing(thing);
-				copyStack.push(copy);
-				return copy;
-			};
-		}
+                function makeCloner (cloneThing) {
+                        return function(thing, thingStack, copyStack) {
+                                thingStack.push(thing);
+                                var copy = cloneThing(thing);
+                                copyStack.push(copy);
+                                return copy;
+                        };
+                }
 
-		function clonePrimitive (primitive) {
-			return primitive;
-		}
+                function clonePrimitive (primitive) {
+                        return primitive;
+                }
 
-		function cloneRegExp (regexp) {
-			return new RegExp(regexp);
-		}
+                function cloneRegExp (regexp) {
+                        return new RegExp(regexp);
+                }
 
-		function cloneDate (date) {
-			return new Date(date.getTime());
-		}
+                function cloneDate (date) {
+                        return new Date(date.getTime());
+                }
 
-		// We can't really clone functions but we can wrap them in a new function that will
-		// recieve clones of any properties the original function may have had
-		function cloneFunction (fn) {
-			return wrapFunctionWithArity(fn);
-		}
+                // We can't really clone functions but we can wrap them in a new function that will
+                // recieve clones of any properties the original function may have had
+                function cloneFunction (fn) {
+                        return wrapFunctionWithArity(fn);
+                }
 
-		// This will not properly clone `constructed` objects because
-		// it is impossible to know with what arguments the constructor
-		// was originally invoked.
-		function cloneObject (object) {
-			return Object.create(Object.getPrototypeOf(object));
-		}
+                // This will not properly clone `constructed` objects because
+                // it is impossible to know with what arguments the constructor
+                // was originally invoked.
+                function cloneObject (object) {
+                        return Object.create(Object.getPrototypeOf(object));
+                }
 
-		function cloneArray (array) {
-			return [];
-		}
+                function cloneArray (array) {
+                        return [];
+                }
 
-		function cloneTypedArray (typedArray) {
-			var len = typedArray.length;
-			return new typedArray.constructor(len);
-		}
+                function cloneTypedArray (typedArray) {
+                        var len = typedArray.length;
+                        return new typedArray.constructor(len);
+                }
 
-		function makeRecursiveCloner (cloneThing, propertyFilter) {
-			return function(thing, thingStack, copyStack) {
-				var clone = this;
+                function makeRecursiveCloner (cloneThing, propertyFilter) {
+                        return function(thing, thingStack, copyStack) {
+                                var clone = this;
 
-				return Object.getOwnPropertyNames(thing)
-					.filter(function(prop){
-						return !propertyFilter || propertyFilter.indexOf(prop) === -1;
-					})
-					.reduce(function(copy, prop) {
-						var thingOffset = thingStack.indexOf(thing[prop]);
+                                return Object.getOwnPropertyNames(thing)
+                                        .filter(function(prop){
+                                                return !propertyFilter || propertyFilter.indexOf(prop) === -1;
+                                        })
+                                        .reduce(function(copy, prop) {
+                                                var thingOffset = thingStack.indexOf(thing[prop]);
 
-						if (thingOffset === -1) {
-							copy[prop] = clone(thing[prop]);
-						} else {
-							copy[prop] = copyStack[thingOffset];
-						}
+                                                if (thingOffset === -1) {
+                                                        copy[prop] = clone(thing[prop]);
+                                                } else {
+                                                        copy[prop] = copyStack[thingOffset];
+                                                }
 
-						return copy;
-					}, cloneThing(thing, thingStack, copyStack));
-			};
-		}
+                                                return copy;
+                                        }, cloneThing(thing, thingStack, copyStack));
+                        };
+                }
 
-		return function _ultraDeepClone (source) {
+                return function _ultraDeepClone (source) {
 
-			var thingStack = [];
-			var copyStack = [];
+                        var thingStack = [];
+                        var copyStack = [];
 
-			function clone (thing) {
-				var typeOfThing = Object.prototype.toString.call(thing);
-				return cloneFunctions[typeOfThing].call(clone, thing, thingStack, copyStack);
-			};
+                        function clone (thing) {
+                                var typeOfThing = Object.prototype.toString.call(thing);
+                                return cloneFunctions[typeOfThing].call(clone, thing, thingStack, copyStack);
+                        };
 
-			return clone(source);
-		};
+                        return clone(source);
+                };
 }));
 
 },{}],16:[function(require,module,exports){
@@ -18340,9 +18340,9 @@ WYMeditor.NativeEditRegistration.prototype._onAnyNativeEdit = function () {
     }
 
     // Non-native modifications are registered when they are performed.
-    // Contrary to those, native edits (e.g. typing) aren't registered 
-    // until word boundaries are reached (space, enter) or edit actions 
-    // (backspace, delete, cut, paste, etc). 
+    // Contrary to those, native edits (e.g. typing) aren't registered
+    // until word boundaries are reached (space, enter) or edit actions
+    // (backspace, delete, cut, paste, etc).
     // This handles an undo after e.g. only part of word is typed.
     //
     // A state with unregistered modifications will be lost upon an undo
@@ -22817,7 +22817,7 @@ rangy.createModule("DomUtil", function(api, module) {
     var dom = api.dom;
     var DomPosition = dom.DomPosition;
     var DOMException = api.DOMException;
-    
+
     /*----------------------------------------------------------------------------------------------------------------*/
 
     // Utility functions
